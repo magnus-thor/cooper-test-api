5b78d0e3a492ed3f7864b2921e2f0a0c06bcf932
magnus-thor <magnus.thor.ar@gmail.com>
2017-10-26 11:40:27 +0200

    rspec for performance data are green

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,4 +52,8 @@ RSpec.describe User, type: :model do
       it { is_expected.to allow_value(email).for(:email) }
     end
   end
+
+  describe 'Relations' do
+    it { is_expected.to have_many :performance_data }
+  end
 end
